141fcba65ab76f014be2b3c36a20c9d803ca505b
cvVal <cvvaldez_00@hotmail.com>
2025-07-10 16:38:15 -0400

    add bunker health system

--- Assets/Scripts/Bunker.cs
+++ Assets/Scripts/Bunker.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Bunker : MonoBehaviour
 {
+
+    [SerializeField] int health = 3; // Health of the bunker
+
+    SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component attached to this GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the object that entered the trigger is a player laser
+        if (collision.CompareTag("PlayerLaser"))
+        {
+            Destroy(collision.gameObject); // Destroy the laser that hit the bunker
+            health--; // Decrease the health of the bunker
+            switch (health)
+            {
+                case 2:
+                    spriteRenderer.color = Color.yellow; // Change the color of the bunker to yellow when health is 2
+                    break;
+                case 1:
+                    spriteRenderer.color = Color.red; // Change the color of the bunker to red when health is 1
+                    break;
+                case 0:
+                    Destroy(gameObject); // Destroy the bunker if health is zero
+                    break;
+            }
+        }
     }
 }
